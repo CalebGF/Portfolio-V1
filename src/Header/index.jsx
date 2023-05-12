c792728f6e8d985b8cd0f4e995caad98ef859c83
CalebGF <39348161+CalebGF@users.noreply.github.com>
2023-05-13 01:14:21 +0200

    Refactoring the Header and adding gh-pages dependency

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -7,54 +7,40 @@ import LanguageSelector from '../LanguageSelector';
 function Header() {
     const { t } = useContext(LanguageContext);
     return (
-        <header className="container-fluid">
-            
-            <nav className="navbar navbar-expand-lg navbar-dark fixed-top navbar-overlay navbar-transparent row">
+        <header >
+            <nav className="navbar navbar-expand-lg navbar-dark fixed-top ">
                 <div className="container-fluid">
-                    <div className="row">
-                        <div className="col-4">
-                            <a className="navbar-brand" href="#">
-                                <img src="src/assets/images/logo.png" alt="" width="40" height="34" className="d-inline-block align-text-top"/>
-                                Caleb González Flores
-                            </a>
-                        </div>
+                    <a className="navbar-brand " href="#">
+                        <img src="src/assets/images/logo.png" alt="" width="40" height="34" className="d-inline-block align-text-top"/>
+                        Caleb González Flores
+                    </a>
 
-                        <div className="col-8">
-                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
-                            </button>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
-                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                                <ul className="navbar-nav me-auto my-2 mb-lg-0 text-center">
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="#aboutMe">{t('aboutMe')}</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="#skills">{t('skills')}</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="#projects">{t('projects')}</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="#contact">{t('contact')}</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="#contact"><LanguageSelector /></a>
-                                    </li>
-                                </ul>
-                            </div>
-
-                        </div>
-
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 text-center">
+                            <li className="nav-item">
+                                <a className="nav-link" href="#aboutMe">{t('aboutMe')}</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#skills">{t('skills')}</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#projects">{t('projects')}</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#contact">{t('contact')}</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#contact"><LanguageSelector /></a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </nav>
-
-
-            <div className="welcome-header">
-
-                <h1>{t('welcome')}</h1>
-            </div>
+            <h1>{t('welcome')}</h1>
         </header>
     );
 }
